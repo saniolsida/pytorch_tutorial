903b4ca43ae6f74355ce4c8190933d543ca642c1
songsan <22000375@handong.ac.kr>
2026-08-10 16:42:18 +0900

    add 05 logistic regression

--- 05_Logistic_regression.py
+++ 05_Logistic_regression.py
@@ -2,3 +2,68 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+
+torch.manual_seed(1)
+
+x_data = [[1, 2], [2, 3], [3, 1], [4, 3], [5, 3], [6, 2]]
+y_data = [[0], [0], [0], [1], [1], [1]]
+
+x_train = torch.FloatTensor(x_data)
+y_train = torch.FloatTensor(y_data)
+
+# print(x_train.shape)
+# print(y_train.shape)
+
+# W = torch.zeros((2, 1), requires_grad=True)
+# b = torch.zeros(1, requires_grad=True)
+
+# optimizer = optim.SGD([W, b], lr=1)
+
+# nb_epochs = 1000
+# for epoch in range(nb_epochs + 1):
+#     # hypothesis = 1 / (1 + torch.exp(-(x_train.matmul(W) + b)))
+#     hypothesis = torch.sigmoid(x_train.matmul(W) + b)
+#     cost = F.binary_cross_entropy(hypothesis, y_train)
+
+#     optimizer.zero_grad()
+#     cost.backward()
+#     optimizer.step()
+
+#     if epoch % 100 == 0:
+#         print(f"cost: {cost.item():.6f}")
+#         # print(hypothesis[:5])
+
+# prediction = hypothesis >= torch.FloatTensor([0.5])
+# correct_prediction = prediction.float() == y_train
+
+# print(correct_prediction[:5])
+
+
+class BinaryClassfier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(2, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        return self.sigmoid(self.linear(x))
+
+
+model = BinaryClassfier()
+optimizer = optim.SGD(model.parameters(), lr=1)
+
+nb_epochs = 100
+for epoch in range(nb_epochs + 1):
+    hypothesis = model(x_train)
+
+    cost = F.binary_cross_entropy(hypothesis, y_train)
+
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+
+    if epoch % 10 == 0:
+        prediction = hypothesis >= torch.FloatTensor([0.5])
+        correct_prediction = prediction.float() == y_train
+        accuracy = correct_prediction.sum().item() / len(correct_prediction)
+        print(f"Cost: {cost.item():.6f} Accuracy {(accuracy * 100):.2f}")
